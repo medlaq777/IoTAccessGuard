8248f0161477e2369efb79562f1f6134de050613
medlaq777 <m.laqsoumi@gmail.com>
2025-05-29 11:44:40 +0100

    Fix the card , and now the qrcode and card is work perfectly and open the doors

--- app/Console/Commands/MqttSubscribe.php
+++ app/Console/Commands/MqttSubscribe.php
@@ -32,31 +32,42 @@ class MqttSubscribe extends Command
 
             $mqtt->connect();
 
-        $mqtt->subscribe($topic, function ($topic, $message) use ($mqtt) {
-            $this->info("Sub => {$topic} => {$message}");
-            $decodedMessage = json_decode($message, true);
-            if (isset($decodedMessage["resource"]) && $decodedMessage["resource"] === 'qrcode') {
-                $payload = json_encode([
-                    "resource" => "qrcode",
-                    "serial" => 205074,
-                    "data" => [
-                        "ret" => 0,
-                        "message" => "ok",
-                    ]
-                ]);
-                $mqtt->publish('test', $payload, 2);
-                $this->info("Published message to topic 'test': " . $payload);
-                $mqtt->unsubscribe($topic);
-            }
-        }, 0);
+            $mqtt->subscribe($topic, function ($topic, $message) use ($mqtt) {
+                $this->info("Sub => {$topic} => {$message}");
+                $decodedMessage = json_decode($message, true);
+                if (isset($decodedMessage["resource"]) && $decodedMessage["resource"] === 'qrcode') {
+                    $payload = json_encode([
+                        "resource" => "qrcode",
+                        "serial" => 205074,
+                        "data" => [
+                            "ret" => 0,
+                            "message" => "ok",
+                        ]
+                    ]);
+                    $mqtt->publish('test', $payload, 2);
+                    $this->info("Published message to topic 'test': " . $payload);
+                    $mqtt->unsubscribe($topic);
+                }else {
+                    $payload = json_encode([
+                        "resource" => "cardno",
+                        "data" => [
+                            "ret" => 0,
+                            "message" => "ok",
+                        ]
+                    ]);
+                    $mqtt->publish('test', $payload, 2);
+                    $this->info("Published message to topic 'test': " . $payload);
+                    $mqtt->unsubscribe($topic);
+                }
+            }, 0);
 
-        while (true) {
-            $mqtt->loop();
+            while (true) {
+                $mqtt->loop();
+            }
+        } catch (ProtocolNotSupportedException $e) {
+            $this->error("Protocol not supported: " . $e->getMessage());
+        } catch (GlobalException $e) {
+            $this->error("An error occurred: " . $e->getMessage());
         }
-    } catch (ProtocolNotSupportedException $e) {
-        $this->error("Protocol not supported: " . $e->getMessage());
-    } catch (GlobalException $e) {
-        $this->error("An error occurred: " . $e->getMessage());
     }
-}
 }
